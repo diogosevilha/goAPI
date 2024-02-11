7351b4691c690b34d5ffa0794ca00f426c467cd9
Diogo Sevilha Couto <diogo.couto@alumni.usp.br>
2024-02-11 16:44:11 -0300

    feat: logger logic

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/diogosevilha/goLangProjects/router"
+import (
+	"fmt"
+
+	"github.com/diogosevilha/goLangProjects/config"
+	"github.com/diogosevilha/goLangProjects/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
